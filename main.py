3b3e201a7e8fe1a8151087e734adf3f406e39920
MadeleineHarbom <madeleine.harbom@gmail.com>
2025-02-07 14:04:47 +0100

    Added path to get request and added post request

--- main.py
+++ main.py
@@ -29,14 +29,26 @@ class GreetRequest(BaseModel):
 async def greet():
     return {"data": items["greeting"]}
 
-@app.get("/greet/{country_code}")
+@app.get("/greet/local/{country_code}")
 async def greet(country_code):
     return {"data": local_greetings[country_code]}
 
 
 @app.post("/greet")
 async def greet(request: GreetRequest):
-    return {"data": f"Hello, {request.name}"}
+    return {"data": f"Hello, Sir {request.name}"}
+
+
+@app.get("/greet/personal/")
+async def greet(country_code: str=None, name: str=None):
+    if country_code and name:
+        return f"{local_greetings[country_code]}, {name}"
+    elif country_code:
+        return f"{local_greetings[country_code]}"
+    elif name:
+        return f"Heya, {name}"
+    else:
+        return "Sup bro?"
 
 
 if __name__ == "__main__":
